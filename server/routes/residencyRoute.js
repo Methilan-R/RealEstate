d856a8833170dc5791be27adf3f2cda4d7ec0669
Methilan-R <rmethilan@gmail.com>
2025-05-29 20:18:46 +0530

    hhh

--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -6,4 +6,4 @@ router.post("/create", createResidency);
 
 router.get("/allresd", getAllResidencies)
 router.get("/:id", getResidency)
-export {router as residencyRoute}
+export {router as residencyRoute} 
